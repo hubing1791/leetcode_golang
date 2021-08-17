e28a6201f5f0db6d2e4d10d35fa3d8fb10cf9032
pengyuan <pengyuan@sensetime.com>
2021-08-17 10:31:02 +0800

    solution

--- extra_practice/small_try/small_try.go
+++ extra_practice/small_try/small_try.go
@@ -2,13 +2,24 @@ package main
 
 import (
 	"fmt"
-	"reflect"
 )
 
+func mapChange(m map[int]int){
+	m[1]=5
+	m[5]=1
+}
+
 func main() {
-	strTry := "abcdefg"
-	fmt.Printf("%+v,		%v\n", strTry[0],reflect.TypeOf(strTry[0]))
-	for _,char :=range strTry {
-		fmt.Printf("%+v,		%v\n",char,reflect.TypeOf(char))
-	}
+	//不同的遍历字符串的方式，返回值不同
+	//strTry := "abcdefg"
+	//fmt.Printf("%+v,		%v\n", strTry[0],reflect.TypeOf(strTry[0]))
+	//for _,char :=range strTry {
+	//	fmt.Printf("%+v,		%v\n",char,reflect.TypeOf(char))
+	//}
+	//
+
+	var mapTry map[int]int
+	mapTry = make(map[int]int)
+	mapChange(mapTry)
+	fmt.Println(mapTry)
 }
